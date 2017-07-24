dd48bbb009d44b9576a73ce57bbcd52fdeb51373
michaelkatcher <30413693+michaelkatcher@users.noreply.github.com>
2017-07-24 15:21:22 -0400

    Removed comments from previous merge

--- grab_sheets.py
+++ grab_sheets.py
@@ -21,28 +21,28 @@ from openpyxl import load_workbook
 
 def date_from_file_name(file_name, return_datetime=False):
     """
-    Return full date as a string from a date in a filename.  # PEP 257 - Docstrings should use imperative.
+    Return full date as a string from a date in a filename.
 
     Args:
         file_name (str): Formatted as: Trade E-mail Archive - yyyy.mm.dd.xlsm
-        return_type (bool, optional): TRUE if return should be datetime  # Should be return_datetime to match function argument
+        return_datetime (bool, optional): TRUE if return should be datetime
                                       FALSE if return should be string
 
     Returns:
         str or datetime: The return value. Date from filename as yyyy.mm.dd
     """
     if return_datetime:
-        year = int(file_name[-15:-11])  # you can use regular expressions or something similar to make this reusable
-        month = int(file_name[-10:-8])  # But hard coding it wont be a problem right now if you always have the same format
+        year = int(file_name[-15:-11])
+        month = int(file_name[-10:-8])
         day = int(file_name[-7:-5])
-        return datetime(year, month, day)  # PEP 8 whitespace
+        return datetime(year, month, day)
     else:
         return file_name[-15:-5]
 
 
-def copy_worksheet(ws_input, ws_output):  # PEP 8 whitespace
+def copy_worksheet(ws_input, ws_output):
     """
-    Copy values from one openpyxl Worksheet to another.  # See above docstring
+    Copy values from one openpyxl Worksheet to another.
 
      Args:
          ws_input (Worksheet): The worksheet the data is being copied from.
@@ -59,17 +59,16 @@ class OutputWorkbook(Workbook):
 
         Args:
             filename (str): The path and filename where the output workbook is saved.
-
         """
         self._filename = filename
 
-        super(OutputWorkbook,self).__init__(write_only=True)  # i think you can just use super() without arguments, but im not sure
+        super(OutputWorkbook,self).__init__(write_only=True)
 
     def __enter__(self):
         return self
 
     def __exit__(self, *args):
-        print 'Saving %s... ' % self._filename.split('\\')[-1]  # didn't realize u use python 2 until now :D
+        print 'Saving %s... ' % self._filename.split('\\')[-1]
         self.save(filename=self._filename)
         print 'Save Complete!'
 
@@ -83,12 +82,12 @@ def main():
     save_path = 'H:\\Test.xlsx' # The path of the output file
 
     # Generate list of filenames
-    include_file = lambda name: name.endswith('.xlsm') and start_date <= date_from_file_name(name,True) <= end_date
+    def include_file(name): return name.endswith('.xlsm') and start_date <= date_from_file_name(name,True) <= end_date
     file_names = [name for name in listdir(data_folder) if include_file(name)]
 
     with OutputWorkbook(save_path) as wb_output:
         for idx,file_name in enumerate(file_names):
-            print 'Processing File ' + str(idx + 1) + ' of ' + str(len(file_names))
+            print 'Processing File %s of %s' % (idx + 1, len(file_names))
 
             ws_output = wb_output.create_sheet(date_from_file_name(file_name))
             ws_input = load_workbook(data_folder + file_name, read_only=True)[sheet_name]
